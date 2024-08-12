e987128f66e8900ecf3de8643997097ac583a36d
Data Mining <dario.andreu@gmail.com>
2024-08-11 21:22:52 -0400

    trabajando poco a poco el control de actv

--- src/cultura/control_actcult/schemas/control_actcult.schema.ts
+++ src/cultura/control_actcult/schemas/control_actcult.schema.ts
@@ -1,6 +1,9 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Types } from "mongoose";
 import { Talento_Artistico_Contratado_Entity } from "../../talentos/talento_contratado/talento_contratado.entity";
+import { Entity } from "src/modules/entity/domain/entities/entity.entity";
+import { EntityModel } from "src/modules/entity/infrastructure/entity.schema";
+import { Type } from "class-transformer";
 
 @Schema({ timestamps:true, collection:'Control_ActividadCultural'})
 export  class Control_ActividadCultural_Model{
@@ -14,6 +17,10 @@ export  class Control_ActividadCultural_Model{
 
     @Prop()
     hora_actcult:string  //Timestamp
+
+    @Prop({ type:Types.ObjectId, ref:EntityModel.name})
+    @Type(()=>EntityModel)
+    Entidad_Responsable:EntityModel
 
     @Prop([Talento_Artistico_Contratado_Entity])
     talentos_y_apoyos?:Talento_Artistico_Contratado_Entity[]
